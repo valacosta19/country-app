36aeb21fa436c9c1b5bf47d7d6af8b5307e841a3
Valentina Acosta <valen.acosta19@hotmail.com>
2024-01-30 21:54:37 -0300

    first commit

---
+++ src/components/CountryItem.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { defineProps } from "vue";
+import { Country } from "../models/country.model";
+
+interface Props {
+  country: Country;
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+  <div class="h-56 sm:h-36">
+    <img
+      class="h-56 sm:h-36 w-full"
+      :src="country.flags.png"
+      :alt="country.flags.alt"
+    />
+  </div>
+  <h4 class="text-xl font-semibold text-gray-500">{{ country.name.common }}</h4>
+  <p class="text-sm text-gray-400">
+    Capital:
+    <span class="font-semibold" v-for="capital in country.capital">
+      {{ capital }}
+      <template>, </template>
+    </span>
+  </p>
+  <p class="text-sm text-gray-400">
+    Population:
+    <span class="font-semibold">
+      {{ country.population?.toLocaleString() }}
+    </span>
+  </p>
+  <p class="text-sm text-gray-400">
+    Population:
+    <span class="font-semibold">
+      {{ country.region }}
+    </span>
+  </p>
+</template>
---
+++ src/components/PageHeader.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="text-center my-24">
+    <h1 class="text-4xl font-bold tracking-wide text-sky-700">Countries App</h1>
+    <h6 class="mt-4 text-xl">A simple app to search for countries</h6>
+  </div>
+</template>
